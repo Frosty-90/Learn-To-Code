bb630e3455a18664c72a22009254fea5d4cd38d7
Frosty-90 <idcwhatyoulike@gmail.com>
2025-04-21 20:45:01 +0530

    final header change

--- Pokedex/src/components/Header.jsx
+++ Pokedex/src/components/Header.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
-function Header() {
+export default function Header(props) {
+  const { handleToggleMenu } = props
   return (
-    <header></header>
+      <header>
+          <button onClick={handleToggleMenu} className="open-nav-button">
+              <i className="fa-solid fa-bars"></i>
+          </button>
+          <h1 className="text-gradient">Pokédex</h1>
+      </header>
   )
 }
-
-export default Header
